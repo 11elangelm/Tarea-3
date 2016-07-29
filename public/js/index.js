7d341ac884a363ad72d8609a37d50506ec8eb9d4
Angel Morales <11elangelm@gmail.com>
2016-07-28 23:58:47 -0600

    Final

--- public/js/index.js
+++ public/js/index.js
@@ -1,8 +1,8 @@
 var combobox = document.getElementById('opciones');
 function actualizarHora(){
-	var d = new Date(); // for now
-	var h = d.getHours(); // => 9
-	var m = d.getMinutes(); // =>  30
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
 	var s = d.getSeconds();
 	cambiarHora(h);
 	cambiarMinuto(m);
@@ -31,10 +31,35 @@ function cambiarSegundo(s){
 	segundera.className += 'ms-'+s;
 }
 
-setInterval(actualizarHora,1000);
-
-combobox.addEventListener('change',changeClock,combobox);
-function changeClock(){
-	var str = combobox.options[combobox.selectedIndex].text;
-	console.log(str);
+function actualizarDigital(){
+	var d = new Date();
+	var h = d.getHours();
+	var m = d.getMinutes();
+	var s = d.getSeconds();
+	document.getElementById('hora').innerHTML = h;
+	document.getElementById('minuto').innerHTML = m;
+	document.getElementById('segundo').innerHTML = s;
 }
+
+combobox.addEventListener('change',changeClock);
+function changeClock(){
+	var combobox = document.getElementById('opciones');
+	var str = combobox.options[combobox.selectedIndex].text;
+	if (str == "Análogo"){
+		setInterval(actualizarHora,1000);
+		digital = document.getElementById('digital');
+		digital.className ="";
+		digital.className += "hidden";
+		analogo = document.getElementById('analogo');
+		analogo.className ="";
+	} else{
+		setInterval(actualizarDigital,1000);
+		analogo = document.getElementById('analogo');
+		analogo.className ="";
+		analogo.className += "hidden";
+		digital = document.getElementById('digital');
+		digital.className ="";
+	}
+}
+
+changeClock();
